1596e39f05585c89c1ad4db475f87891daf3b6b0
alice <alice@alysia.au>
2025-04-13 17:29:54 +1000

    troubleshooting of the tsconfig errors.

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import { App } from "astal/gtk4"
-import style from "./style.scss"
-import Bar from "./widget/Bar"
+import style from "./styles/style.scss"
+import Bar from "./widgets/Bar"
 
 App.start({
     css: style,
